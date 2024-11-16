5093b9f97d8358a825a59e0dfb64205e4d95368b
JaysonTurit <jayson@turit.co>
2024-11-16 15:52:41 -0500

    Nueva experiencia

--- src/components/compMedioSection/Experiencia.vue
+++ src/components/compMedioSection/Experiencia.vue
@@ -10,7 +10,6 @@
           </div>
 
           <div class="experienciaContenido">
-<<<<<<< HEAD
             <h2 class="cargoTitulo">Desarrollador Full Stack</h2>
             <p>Turit</p>
             <p class="fechaExperiencia">Abril 2024 - Octubre 2024</p>
@@ -18,10 +17,6 @@
           <div class="experienciaContenido">
             <h2 class="cargoTitulo">Desarrollador Full Stack</h2>
             <p>Turit</p>
-=======
-            <h2 class="cargoTitulo">Desarrollador de Software</h2>
-            <p>Meltec Comunicaciones</p>
->>>>>>> 1b388e8eb9db3e0a61530a6833e38570dfe62ac5
             <p class="fechaExperiencia">Abril 2024 - Octubre 2024</p>
           </div>
         </section>
@@ -53,15 +48,11 @@ main {
 }
 
 #experiencia {
-<<<<<<< HEAD
   width: 800px;
   display: flex; /* Cambia a flex para colocar en filas horizontales */
   gap: 80px; /* Espacio entre cada columna */
   position: absolute;
   left: -130px;
-=======
-  width: 100%;
->>>>>>> 1b388e8eb9db3e0a61530a6833e38570dfe62ac5
 }
 
 .experienciaContenido {
@@ -70,15 +61,8 @@ main {
   flex-direction: column;
   align-items: flex-start;
   text-align: left;
-<<<<<<< HEAD
   padding: 20px 30px;
   border-right: 2px solid #ccc;
-=======
-  margin: 0;
-  padding: 20px 30px;
-  position: relative; /* Posicionamiento relativo para cada contenedor */
-  border-right: 2px solid #ccc; /* Línea vertical en el lado derecho */
->>>>>>> 1b388e8eb9db3e0a61530a6833e38570dfe62ac5
 }
 
 .cargoTitulo {
