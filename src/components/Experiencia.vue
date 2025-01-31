f9ce05d1c73e1c211dac2a8097c84c1f2c289b04
Jayson Vargas <vjaison30@gmail.com>
2025-01-30 22:30:45 -0500

    Se ha implementado una nueva estructura del componente de experiencia

--- src/components/Experiencia.vue
+++ src/components/Experiencia.vue
@@ -1,90 +1,98 @@
 <template>
-  <section class="experience-section">
-    <h1 class="section-title">Experiencia Profesional</h1>
-    <div class="experience-container">
-      <div
-        v-for="(experience, index) in experiences"
-        :key="index"
-        class="experience-card"
-        @mouseenter="hoverExperience(index)"
-        @mouseleave="leaveExperience"
-        @click="toggleDetails(index)"
-      >
+  <div>
+    <div class="principal-container">
+      <h1 class="tituloSeccion">Experiencia profesional</h1>
+      <div class="card">
+        <div class="icon-container">
+          <font-awesome-icon icon="fas fa-laptop-code" />
+        </div>
+
         <div class="card-content">
-          <div class="icon-container">
-            <i :class="experience.icon"></i>
+          <h1>Desarrollador Full-Stack</h1>
+          <h4>Turit</h4>
+          <p class="fecha">Octubre 2024 - Diciembre 2024</p>
+
+          <div class="description">
+            <h3>Responsabilidades:</h3>
+            <p>
+              Desarrollo de interfaces web interactivas y optimización de flujos
+              de trabajo internos.
+            </p>
+            <p>
+              Implementación de soluciones no-code/low-code con herramientas
+              como Bubble.io, Make, Zapier y Airtable.
+            </p>
+            <p>Gestión de proyectos y control de versiones con GitHub.</p>
           </div>
-          <h2 class="experience-title">{{ experience.title }}</h2>
-          <p class="experience-company">{{ experience.company }}</p>
-          <p class="experience-duration">
-            <i class="fas fa-calendar-alt"></i> {{ experience.duration }}
-          </p>
-          <ul class="experience-description">
-            <li v-for="(desc, index) in experience.description" :key="index">
-              {{ desc }}
-            </li>
-          </ul>
+
+          <div class="skills">
+            <ul>
+              <li><p>Vue.js</p></li>
+              <li><p>JavaScript</p></li>
+              <li><p>CSS</p></li>
+            </ul>
+          </div>
+          <div class="buttons">
+            <button class="botonContactar">
+              <a href="#contacto"><span>Conoce mi experiencia</span></a>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="icon-container">
+          <font-awesome-icon icon="fas fa-laptop-code" />
+        </div>
+
+        <div class="card-content">
+          <h1>Desarrollador de software</h1>
+          <h4>Meltec</h4>
+          <p class="fecha">Marzo 2024 - Octubre 2024</p>
+
+          <div class="description">
+            <h3>Responsabilidades:</h3>
+            <p>
+              Desarrollo de aplicaciones web e intranet corporativa con Vue.js,
+              HTML, CSS y JavaScript, creando interfaces intuitivas y mejorando
+              la comunicación interna.
+            </p>
+            <p>
+              Automatización de procesos mediante herramientas de IA (Make) y
+              desarrollo de un bot conversacional con Voiceflow para soporte
+              técnico automatizado.
+            </p>
+            <p>Gestión de proyectos y control de versiones con GitHub.</p>
+          </div>
+
+          <div class="skills">
+            <ul>
+              <li><p>Vue.js</p></li>
+              <li><p>JavaScript</p></li>
+              <li><p>CSS</p></li>
+            </ul>
+          </div>
+          <div class="buttons">
+            <button class="botonContactar">
+              <a href="#contacto"><span>Conoce mi experiencia</span></a>
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faArrowRightLong,
-  faBolt,
-  faCogs,
-  faDesktop,
-  faFileAlt,
-  faFontAwesome,
-  faMagnifyingGlass,
-  faNetworkWired,
-  faPuzzlePiece,
-  faRobot,
-  faRocket,
-  faSquareUpRight,
-  faTools,
-} from "@fortawesome/free-solid-svg-icons";
-import { faMinus } from "@fortawesome/free-solid-svg-icons";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
-import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { faLaptopCode, faPalette } from "@fortawesome/free-solid-svg-icons";
 
-library.add(
-  faMagnifyingGlass,
-  faArrowLeft,
-  faPlay,
-  faAnglesDown,
-  faGithubAlt,
-  faArrowRight,
-  faMinus,
-  faArrowRightLong,
-  faLaptopCode,
-  faRocket,
-  faSquareUpRight,
-  faCogs,
-  faDesktop,
-  faBolt,
-  faPuzzlePiece,
-  faFileAlt,
-  faNetworkWired,
-  faTools,
-  faRobot
-);
+library.add(faLaptopCode, faPalette);
+
 export default {
   name: "Habilidades",
   components: {
     FontAwesomeIcon,
   },
-  props: {
-    showContent: Object,
-  },
-
   data() {
     return {
       experiences: [
@@ -93,14 +101,15 @@ export default {
           company: "Turit",
           duration: "Octubre 2024 - Diciembre 2024",
           description: [
-            "🚀 Gestión de procesos internos: Desarrollo y gestión de procesos internos con CRM (GoHighlevel) optimizando flujos y reportes",
+            "🚀 Gestión de procesos internos: Desarrollo y gestión de procesos internos con CRM (GoHighlevel) optimizando flujos y reportes.",
             "💻 Desarrollo web responsivo: Creación de interfaces funcionales con HTML, CSS, JavaScript y Bootstrap.",
             "🔧 Optimización de tareas: Mejora de la productividad mediante Google Apps Script.",
-            "🌐 Soluciones no-code/low-code: Implementación de soluciones con Bubble.io, Airtable y automatizaciones con Make, Zapier y Pipedream, creando sitios web dinámicos y funcionales.",
+            "🌐 Soluciones no-code/low: Implementación de soluciones no-code/low-code con Bubble.io, Airtable y automatizaciones con Make, Zapier y Pipedream, creando sitios web dinámicos y funcionales.",
             "📝 Documentación y mantenimiento: Gestión de proyectos y control de versiones mediante GitHub.",
           ],
           icon: "fas fa-laptop-code",
           skills: [{ name: "Vue.js" }, { name: "JavaScript" }, { name: "CSS" }],
+          expanded: false,
         },
         {
           title: "Desarrollador de Software",
@@ -109,47 +118,40 @@ export default {
           description: [
             "🚀 Desarrollo web: Creación de aplicaciones con HTML, CSS, JavaScript, Vue.js y Bootstrap para interfaces intuitivas.",
             "🌐 Intranet: Diseño y desarrollo de una intranet con Vue.js para mejorar la comunicación interna.",
-            "🛠️ Aplicación no-code: Implementación de soluciones no-code con AppSheet para optimizar procesos y mantenimiento de páginas con Wordpress.",
+            "🛠️ Aplicación no-code: Implementación de soluciones no-code con AppSheet para optimizar procesos.",
             "🤖 Bot conversacional: Desarrollo de un bot con Voiceflow integrado a APIs externas para mejorar el soporte técnico.",
-            "⚙️ Automatización: Uso de herramientas como Make y Zapier, incrementando la eficiencia en diversas áreas.",
+            "⚙️ Automatización: Uso de herramientas de IA como Make para automatizar tareas manuales y mejorar la eficiencia.",
           ],
           icon: "fas fa-palette",
           skills: [{ name: "Figma" }, { name: "Vue.js" }, { name: "CSS" }],
+          expanded: false,
         },
       ],
-      hoveredIndex: null,
     };
   },
   methods: {
-    hoverExperience(index) {
-      this.hoveredIndex = index;
+    showDetails(index) {
+      // Mostrar el contenido al pasar el mouse sobre la tarjeta
+      this.experiences[index].expanded = true;
     },
-    leaveExperience() {
-      this.hoveredIndex = null;
+    hideDetails(index) {
+      // Ocultar el contenido al quitar el mouse
+      this.experiences[index].expanded = false;
     },
-  },
-  toggleDetails(index) {
-    this.expandedIndex = this.expandedIndex === index ? null : index;
   },
 };
 </script>
 
 <style scoped>
-.experience-section {
-  padding: 2rem;
-  background: #f9f9f9;
-}
-
-.section-title {
-  text-align: center;
-  font-size: 4rem;
-  margin-bottom: 1.5rem;
-  margin-left: -20px;
-  color: #333;
-  overflow: hidden; /* Oculta el texto mientras se "escribe" */
-  border-right: 3px solid var(--primary-color); /* Línea tipo cursor */
-  white-space: nowrap;
-  animation: pulse 2s infinite;
+.principal-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 750px;
+  background-color: var(--color-blanco);
+  flex-wrap: wrap;
+  gap: 40px;
+  position: relative;
 }
 
 @keyframes pulse {
@@ -161,311 +163,185 @@ export default {
     transform: scale(1.02);
   }
 }
-.experience-container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-  justify-content: center;
-}
 
-.experience-card {
-  background: linear-gradient(135deg, #ffffff, #f0f0f0);
-  border-radius: 15px;
-  padding: 1.5rem;
-  width: 400px;
+.tituloSeccion {
+  font-size: 60px;
+  color: var(--color-negro);
 
-  transition: transform 0.4s ease, box-shadow 0.4s ease, opacity 0.3s ease;
   text-align: center;
-  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
-  position: relative;
-  overflow: hidden;
-  animation: float 3s ease-in-out infinite;
-}
-
-@keyframes float {
-  0%,
-  100% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-5px);
-  }
-}
-
-.experience-card:hover {
-  transform: translateY(-10px);
-  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
-  border: 1px solid #009688;
-}
-
-.experience-card::after {
-  content: "";
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 0;
-  transition: opacity 0.3s ease;
+  top: -10px; /* Ajusta la posición hacia arriba */
+
+  margin-bottom: 15px;
+  animation: pulse 2s infinite;
 }
 
-.experience-card:hover::after {
-  opacity: 1;
-}
-
-.card-content {
+.card {
+  width: 500px;
+  height: 500px;
+  border: 1px solid #e0e0e0;
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   align-items: center;
-  transition: opacity 0.3s ease;
+  background-color: white;
+  position: relative;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 6px 16px rgba(0, 0, 0, 0.15);
+}
+
+.card-content {
+  padding: 24px;
+  text-align: center;
 }
 
 .icon-container {
   font-size: 3rem;
-  background: linear-gradient(135deg, #004e7c, #007f9c);
-  background-clip: initial;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  margin-right: 5px;
-  margin-bottom: 1rem;
+  color: #007f9c;
+  margin-bottom: -10px;
+  margin-top: 15px;
   transition: transform 0.3s ease;
-  transform: rotate(15deg) scale(1.2);
 }
 
-.experience-title {
+.card:hover .icon-container {
+  transform: rotate(15deg);
+}
+
+.description {
+  margin-top: 15px;
+  font-size: 14px;
+  color: #777;
+}
+
+.card-content h1 {
+  color: #555;
+  margin: 0;
   font-size: 1.5rem;
-  color: #333;
 }
 
-.experience-company {
-  margin: 0.5rem 0;
-  color: #666;
-}
-.experience-description {
-  position: relative;
-  padding: 1rem;
-  background-color: #f0f4f7;
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  color: #666;
-  margin-bottom: 1rem;
-  height: 450px;
-}
-
-.experience-description li {
-  list-style: none;
-  margin-bottom: 0.8rem; /* Espaciado entre los elementos de la lista */
-  padding-left: 1rem; /* Espaciado izquierdo para alinear con el texto */
-  position: relative;
-  font-weight: 400;
-  line-height: 1.5;
-}
-
-.experience-description::before {
-  position: absolute;
-  top: -10px;
-  left: 10px;
-  font-size: 2rem;
-}
-.experience-duration {
-  font-style: italic;
-  color: #999;
-  margin-top: 0.5rem;
-}
-
-.experience-skills {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  color: var(--color-gris-fondo);
+.card-content h4 {
+  color: #555;
   font-weight: bold;
+  font-size: 20px;
+  margin: 0;
 }
 
-.spanSkills span {
+.card-content p {
   font-size: 15px;
-  color: #666;
+  color: #555;
+  margin-bottom: 15px;
+  margin: 10px 0;
 }
-
-.skill i {
-  font-size: 15px;
-  color: #009688;
-  transition: transform 0.3s ease;
-}
-
-.skill {
-  margin: 0.5rem;
+.card-content .fecha {
   font-size: 0.9rem;
-  transition: transform 0.3s ease, text-shadow 0.3s ease;
+  color: #777;
+  margin-bottom: 16px;
 }
 
-.skill i {
-  margin-right: 5px;
+.card-content span {
+  font-size: 15px;
+  color: #555;
+  padding: 15px;
 }
 
-.skill:hover i {
-  color: #009688;
+.skills {
+  margin-top: 20px;
+  padding: 5px;
+  border-radius: 5px;
+  display: flex; /* Usamos flexbox para centrar */
+  justify-content: center; /* Centrar horizontalmente */
+  align-items: center;
+  text-align: center;
 }
 
-.skill:hover {
-  text-decoration: underline;
+.skills ul {
+  list-style: none;
+  display: flex; /* Disposición en fila */
+  padding: 0;
+  margin: 0;
+  justify-content: center;
+  gap: 8px;
+  flex-wrap: wrap;
 }
 
-.experience-card:hover .icon-container {
-  transform: rotate(0deg) scale(1.2);
+.skills li {
+  width: 100px;
+  background-color: var(--color-negro); /* Fondo oscuro */
+  padding: -10px;
+  border-radius: 5px;
+  color: white; /* Color de texto blanco para resaltar sobre fondo oscuro */
+  margin: 0 1px; /* Espaciado entre las habilidades */
+}
+
+.skills p {
+  color: var(--color-blanco);
+  font-size: 13px;
+}
+
+.buttons {
+  margin-top: 10px;
+}
+
+.buttons p:hover {
+  color: var(--color-blanco);
+}
+
+.botonContactar {
+  background-color: transparent;
+  border: 1px solid var(--color-negro);
+  color: white;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+}
+.botonContactar a {
+  text-decoration: none;
+  color: var(--color-negro);
+}
+
+.botonContactar:hover {
+  background-color: var(--color-negro);
+  transition: 0.5s;
+}
+
+.botonContactar span {
+  color: var(--color-negro);
+  transform: translateY(-3px);
+}
+.botonContactar:hover a span {
+  color: var(--color-blanco);
 }
 
 @media (max-width: 480px) {
-  .experience-section {
-    padding: 2rem;
-    background: #f9f9f9;
-  }
-
-  .section-title {
-    text-align: start;
-    font-size: 4rem;
-    white-space: normal; /**Para que la letra se baje */
-    margin-bottom: 1.5rem;
-    margin-left: -20px;
-    color: #333;
-  }
-
-  .experience-container {
+  .principal-container {
     display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
     justify-content: center;
-  }
-
-  .experience-card {
-    background: linear-gradient(135deg, #ffffff, #f0f0f0);
-    border-radius: 15px;
-    padding: 1.5rem;
-    width: 400px;
-    transition: transform 0.4s ease, box-shadow 0.4s ease, opacity 0.3s ease;
-    text-align: center;
-    box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
-    position: relative;
-    overflow: hidden;
-  }
-
-  .experience-card:hover {
-    transform: translateY(-10px);
-    box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
-    border: 1px solid #009688;
-  }
-
-  .experience-card::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    opacity: 0;
-    transition: opacity 0.3s ease;
-  }
-
-  .experience-card:hover::after {
-    opacity: 1;
-  }
-
-  .card-content {
-    display: flex;
-    flex-direction: column;
     align-items: center;
-    transition: opacity 0.3s ease;
+    height: 100%;
+    background-color: var(--color-blanco);
+    gap: 10px;
   }
 
-  .icon-container {
-    font-size: 3rem;
-    background: linear-gradient(135deg, #004e7c, #007f9c);
-    background-clip: initial;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    margin-right: 5px;
-    margin-bottom: 1rem;
-    transition: transform 0.3s ease;
-    transform: rotate(15deg) scale(1.2);
-  }
-
-  .experience-title {
-    font-size: 1.5rem;
-    color: #333;
-  }
-
-  .experience-company {
-    color: #666;
-  }
-  .experience-description {
-    position: relative;
-    padding: 1rem;
-    background-color: #f0f4f7;
-    border-radius: 10px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    color: #666;
-    margin-bottom: 1rem;
-    height: 680px;
-  }
-
-  .experience-description li {
-    list-style: none;
-    margin-bottom: 0.8rem; /* Espaciado entre los elementos de la lista */
-    padding-left: 1rem; /* Espaciado izquierdo para alinear con el texto */
-    position: relative;
-    font-weight: 400;
-    line-height: 1.5;
-  }
-
-  .experience-description::before {
-    position: absolute;
-    top: -10px;
-    left: 10px;
-    font-size: 2rem;
-  }
-  .experience-duration {
-    font-style: italic;
-    color: #999;
-    margin-top: 0.5rem;
-  }
-
-  .experience-skills {
+  .tituloSeccion {
     display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    color: var(--color-gris-fondo);
-    font-weight: bold;
+    text-align: start;
+    margin-left: 15px;
+    margin-bottom: 10px;
+    position: relative; /* Asegúrate de que el título tenga un contexto de apilamiento */
+    z-index: 2; /* Un valor mayor que el de las LSA cards */
   }
 
-  .spanSkills span {
-    font-size: 15px;
-    color: #666;
-  }
-
-  .skill i {
-    font-size: 15px;
-    color: #009688;
-    transition: transform 0.3s ease;
-  }
-
-  .skill {
-    margin: 0.5rem;
-    font-size: 0.9rem;
-  }
-
-  .skill i {
-    margin-right: 5px;
-  }
-
-  .skill:hover i {
-    color: #009688;
-  }
-
-  .skill:hover {
-    text-decoration: underline;
-  }
-
-  .experience-card:hover .icon-container {
-    transform: rotate(0deg) scale(1.2);
+  .card {
+    width: 320px;
+    height: 100%;
+    margin-bottom: 20px;
   }
 }
 </style>
